8ee7c7839c06bfd45e64f74443fabc1ec4926297
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2024-03-04 13:35:06 +0300

    Add direct dependencies on blockchain utils

--- src/utils/etherlinkUtils.ts
+++ src/utils/etherlinkUtils.ts
@@ -1,4 +1,4 @@
-import { utils } from 'web3';
+import * as web3Utils from 'web3-utils';
 import * as web3Validator from 'web3-validator';
 
 export const isAddress = (address: string): boolean => web3Validator.isAddress(address, true);
@@ -6,7 +6,7 @@ export const isAddress = (address: string): boolean => web3Validator.isAddress(a
 const isTransactionRegex = /^0x[0-9a-f]{64}$/;
 export const isTransaction = (transactionHash: string) => isTransactionRegex.test(transactionHash);
 
-export const toChecksumAddress = (address: string): string => utils.toChecksumAddress(address);
+export const toChecksumAddress = (address: string): string => web3Utils.toChecksumAddress(address);
 
 export const prepareHexPrefix = (value: string, addOrRemove: boolean): string => {
   return value.startsWith('0x')
